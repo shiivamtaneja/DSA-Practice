3cf889aacae3e26bb24514a68d51e1c344db281c
ShivamTaneja <shiivamtaneja@gmail.com>
2023-03-22 19:58:33 +0530

    Added 9 different patterns

--- DSA/Patterns/Patterns.java
+++ DSA/Patterns/Patterns.java
@@ -92,7 +92,7 @@ public class Patterns {
                     12
                     1
         */
-        /* 
+        /*           
         for(int i = n; i >= 1; i--){
             for(int j = 1; j <= i; j++){
                 System.out.print(j);
@@ -107,6 +107,91 @@ public class Patterns {
                    ***** 
                   *******
                  *********
+        */
+        /* 
+        for(int i = 0; i < n; i++){
+            for(int j =0; j < n - i - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2*i+1; j++){
+                System.out.print("*");
+            }
+            for(int j =0; j < n - i - 1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        */
+
+        /* 
+                 *********
+                  *******
+                   ***** 
+                    ***
+                     *
+        */
+        /*  
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 2*n - 2*i - 1; j > 0; j--){
+                System.out.print("*");
+            }
+            for(int j = 0; j <= i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        */
+
+        /* 
+                     *
+                    ***
+                   ***** 
+                  *******
+                 *********
+                 *********
+                  *******
+                   ***** 
+                    ***
+                     *
+        */
+        /* 
+        for(int i = 0; i < n; i++){
+            for(int j =0; j < n - i - 1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2*i+1; j++){
+                System.out.print("*");
+            }
+            for(int j =0; j < n - i - 1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 2*n - 2*i - 1; j > 0; j--){
+                System.out.print("*");
+            }
+            for(int j = 0; j <= i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        */
+
+        /* 
+                    *
+                    **
+                    ***
+                    ****
+                    ***
+                    **
+                    *
         */
         
     }   
