458f4031576573183c2b37051186e8ee627fe53b
ShivamTaneja <shiivamtaneja@gmail.com>
2023-03-25 01:09:17 +0530

    Add more patterns

--- DSA/Patterns/Patterns.java
+++ DSA/Patterns/Patterns.java
@@ -193,6 +193,32 @@ public class Patterns {
                     **
                     *
         */
+        /* 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < n-1; i++){
+            for(int j = n - 1; j > i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+
+        /* 
+                    1
+                    01
+                    101
+                    0101
+                    10101
+        */
+        for(int i = 0; i < n; i++){
+            
+        }
+        
         
     }   
 }
