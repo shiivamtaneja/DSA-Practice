28febd6b620ebe5a933485286408e3836b49dbc7
Shivam Taneja <shiivamtaneja@gmail.com>
2023-04-07 15:48:44 +0530

    Add the different sorting techniques

---
+++ DSA/Sorting/SelectionSort.java
@@ -0,0 +1,39 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    static void selectionSort(int arr[], int n){
+        for(int i = 0; i <= n-2; i ++){
+            int min = i;
+            for(int j = i; j <= n-1; j++){
+                if(arr[j] < arr[min]){
+	                min = j;
+	            }
+            }
+            int temp = arr[min];
+	        arr[min] = arr[i];
+	        arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements of array: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+ 
+        selectionSort(arr, n);
+        System.out.print("Sorted array is : ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    
+    }
+}
