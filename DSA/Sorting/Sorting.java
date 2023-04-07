84563df479bd212008e403372ed2e08e5a42f83f
Shivam Taneja <shiivamtaneja@gmail.com>
2023-04-07 20:55:54 +0530

    Add insertion sort

--- DSA/Sorting/Sorting.java
+++ DSA/Sorting/Sorting.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Sorting {
 
-    // Selects the minimum 
+    // Selects the minimum and places it 
+    // in correct pos.
     static void selectionSort(int arr[], int n){
         for(int i = 0; i <= n-2; i ++){
             int min = i;
@@ -32,6 +33,20 @@ public class Sorting {
         }
     }
 
+    // Takes the element, and places 
+    // in correct pos.
+    static void insertionSort(int arr[], int n){
+        for(int i = 0; i < n - 1; i++){
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array: ");  
@@ -44,7 +59,8 @@ public class Sorting {
         }
  
         // selectionSort(arr, n);
-        bubbleSort(arr, n);
+        // bubbleSort(arr, n);
+        insertionSort(arr, n);
         System.out.print("Sorted array is : ");
         for(int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
