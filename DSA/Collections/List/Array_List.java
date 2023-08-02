be9f6dbd36d0f634a6f8269671bcdabc14a47eb1
ShivamTaneja <shiivamtaneja@gmail.com>
2023-08-02 19:51:06 +0530

    update: arraylist

--- DSA/Collections/List/Array_List.java
+++ DSA/Collections/List/Array_List.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Array_List {
         
     public static void main(String[] args) {
-    
+        
         // Integer | String | Boolean
         ArrayList<Integer> list = new ArrayList<>();
 
