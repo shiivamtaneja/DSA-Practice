a44f494c1abd80d970c82d333b9285089852749b
ShivamTaneja <shiivamtaneja@gmail.com>
2023-08-13 15:07:39 +0530

    add more recursion functions

--- DSA/Recursion/Recursion.java
+++ DSA/Recursion/Recursion.java
@@ -72,8 +72,60 @@ public class Recursion {
     return n * fact(n - 1);
   }
 
+  /*
+   * Reverse an array - two pointers
+   * 
+   *      [1, 2, 3, 4]
+   *       |        |
+   *       V        V
+   *       l        r
+  */
+  public static void reverseArrayTwoPtr(int[] arr, int l, int r) {
+    if (l >= r)
+      return;
+    int temp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = temp;
+    reverseArrayTwoPtr(arr, l + 1, r - 1);
+  }
+
+  /*
+   * Reverse an array - one pointers
+   * 
+   *      [1, 2, 3, 4]
+   *       |        |
+   *       V        V
+   *       l     n - i - 1
+  */
+  public static void reverseArrayOnePtr(int[] arr, int i) {
+    if (i >= arr.length / 2)
+      return;
+    int temp = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+    reverseArrayOnePtr(arr, i + 1);
+  }
+
+  public static void printArray(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  // Check if string is Palindrome
+  public static boolean checkPalindrome(String s, int i) {
+    if(i >= s.length() / 2) {
+      return true;
+    } 
+    if(s.charAt(i) != s.charAt(s.length() - i - 1)) {
+      return false;
+    }
+    return checkPalindrome(s, i + 1);
+  }
+
   public static void main(String[] args) {
-    int n = 5;
+    // int n = 5;
 
     // printName(1, n);
 
@@ -89,8 +141,20 @@ public class Recursion {
 
     // System.out.println(firstNSumFunctional(n));
 
-    System.out.println(fact(n));
+    // System.out.println(fact(n));
 
+    // int[] arr = { 1, 2, 3, 4 };
+
+    // printArray(arr);
+    // reverseArrayTwoPtr(arr, 0, arr.length - 1);
+    // printArray(arr);
+
+    // printArray(arr);
+    // reverseArrayOnePtr(arr, 0);
+    // printArray(arr);
+    
+    String s = "MADAM";
+    System.out.println(checkPalindrome(s, 0));
   }
 
 }
