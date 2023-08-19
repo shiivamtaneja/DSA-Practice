e7e4bacdf93d9d673b7a8383654e92ba5ed4fcf9
ShivamTaneja <shiivamtaneja@gmail.com>
2023-08-19 13:58:26 +0530

    add circular queue using arrays

--- DSA/Queue/Queue.java
+++ DSA/Queue/Queue.java
@@ -2,29 +2,90 @@ package Queue;
 
 public class Queue {
 
-  static class ArrayQueue {
+  // static class ArrayQueue {
+  // static int arr[];
+  // static int size;
+
+  // static int rear = -1;
+
+  // ArrayQueue(int n) {
+  // arr = new int[n];
+  // this.size = n;
+  // }
+
+  // public static boolean isEmpty() {
+  // return rear == -1;
+  // }
+
+  // // Add function - enqueue
+  // public static void add(int data) {
+  // if (rear == size - 1) {
+  // System.out.println("Full Queue");
+  // return;
+  // }
+
+  // rear++;
+  // arr[rear] = data;
+  // }
+
+  // public static int remove() {
+  // if (isEmpty()) {
+  // return -1;
+  // }
+
+  // int front = arr[0];
+
+  // for (int i = 0; i < rear; i++) {
+  // arr[i] = arr[i + 1];
+  // }
+
+  // rear--;
+
+  // return front;
+  // }
+
+  // public static int peek() {
+  // if (isEmpty()) {
+  // return -1;
+  // }
+
+  // return arr[0];
+  // }
+  // }
+
+  static class ArrayCircularQueue {
     static int arr[];
     static int size;
 
     static int rear = -1;
+    static int front = -1;
 
-    ArrayQueue(int n) {
+    ArrayCircularQueue(int n) {
       arr = new int[n];
       this.size = n;
     }
 
     public static boolean isEmpty() {
-      return rear == -1;
+      return rear == -1 && front == -1;
+    }
+
+    public static boolean isFull() {
+      return (rear + 1) % size == front;
     }
 
     // Add function - enqueue
     public static void add(int data) {
-      if (rear == size - 1) {
+      if (isFull()) {
         System.out.println("Full Queue");
         return;
       }
 
-      rear++;
+      // For adding first element
+      if (front == -1) {
+        front = 0;
+      }
+
+      rear = (rear + 1) % size;
       arr[rear] = data;
     }
 
@@ -33,15 +94,18 @@ public class Queue {
         return -1;
       }
 
-      int front = arr[0];
+      int result = arr[front];
 
-      for (int i = 0; i < rear; i++) {
-        arr[i] = arr[i + 1];
+      // if there is only one element
+      if (rear == front) {
+        rear = front = -1;
+      }
+      // else move to the next pos using the special formula
+      else {
+        front = (front + 1) % size;
       }
 
-      rear--;
-
-      return front;
+      return result;
     }
 
     public static int peek() {
@@ -49,20 +113,76 @@ public class Queue {
         return -1;
       }
 
-      return arr[0];
+      return arr[front];
     }
   }
 
   public static void main(String[] args) {
-    ArrayQueue q = new ArrayQueue(5);
+    ArrayCircularQueue q = new ArrayCircularQueue(5);
 
     q.add(1);
     q.add(2);
     q.add(3);
+    q.add(4);
+    q.add(5);
+
+    System.out.println("________");
+
+    System.out.println(q.remove());
+    /*
+     *          [1, 2, 3, 4, 5]
+     *           |           |
+     *         front        rear
+     * 
+     *          [X, 2, 3, 4, 5]
+     *              |        |
+     *            front     rear
+     */
+
+    q.add(6);
+
+    /*
+     *          [X, 2, 3, 4, 5]
+     *              |        |
+     *            front     rear
+     * 
+     *          [6, 2, 3, 4, 5]
+     *           |  |           
+     *        rear  front        
+     */
+
+    System.out.println("________");
+
+    System.out.println(q.remove());
+
+    /*
+     *          [6, 2, 3, 4, 5]
+     *           |  |           
+     *        rear  front
+     * 
+     *          [6, X, 3, 4, 5]
+     *           |     |           
+     *        rear     front
+     */
+
+    q.add(7);
+
+    /*
+     *          [6, X, 3, 4, 5]
+     *           |     |           
+     *        rear     front
+     * 
+     *          [6, 7, 3, 4, 5]
+     *              |  |           
+     *           rear  front       
+     */
+
+    System.out.println("________");
 
     while (!q.isEmpty()) {
       System.out.println(q.peek());
       q.remove();
     }
+
   }
 }
